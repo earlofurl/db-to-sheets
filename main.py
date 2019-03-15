9ef9f178d916cec6e003744fa893f5ff7ae0158e
earlofurl <jackthename@gmail.com>
2019-03-15 11:20:32 -0700

    initial commit of basics. db connection thought unnecessary and skipped

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygsheets
+import psycopg2 as pg
+import pandas as pd
+
+gc = pygsheets.authorize(service_file='db-sync-234405-fa350d5692fd.json')
+
+wks = gc.open("New 2019 Moto Lab Results").sheet1
+
+conn = pg.connect("host='motocloudbase.ctiujpo8nyuy.us-west-2.rds.amazonaws.com' dbname='motocloudbase1' user='motomaster' password='ZXRGJfpMRe3S' port='5432'")
+df = pd.read_sql_query('select * from masterlabresults', con=conn)
+
+wks.set_dataframe(df, (1, 1))
