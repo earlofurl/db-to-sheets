89e67e0d0a6d8e32a2f1b9a2f2cdfb20f39058e3
earlofurl <jackthename@gmail.com>
2019-03-16 19:59:05 -0700

    adapted for AWS lambda with some debug and logging additions comments added

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import botocore
 import pygsheets
 import psycopg2 as pg
 import pandas as pd
+import json  # currently only used to verify that file is being downloaded from S3
+# logging functionality added
 import logging
-import json
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -14,11 +15,13 @@ def lambda_handler(event, context):
     logger.info('got event{}'.format(event))
     logger.error('something went wrong')
 
+    # establish s3 interaction client. provide variables for later actions
     s3 = boto3.client('s3')
     BUCKET_NAME = 'motocloudstor'
     KEY = 'db-sync-234405-fa350d5692fd.json'
     LOCAL_FILENAME = '/tmp/{}'.format(os.path.basename(KEY))
 
+    # try statement contains additional JSON code for verifying in logs that file was downloaded.
     try:
         s3.download_file(Bucket=BUCKET_NAME, Key=KEY, Filename=LOCAL_FILENAME)
         with open(LOCAL_FILENAME, 'r') as handle:
@@ -29,14 +32,16 @@ def lambda_handler(event, context):
             print("The object does not exist.")
         else:
             raise
-
+    # psycopg2 used as db engine for pandas
     conn = pg.connect(host='motocloudbase.ctiujpo8nyuy.us-west-2.rds.amazonaws.com',
                       dbname='motocloudbase1',
                       user='motomaster',
                       password='ZXRGJfpMRe3S',
                       port='5432')
+    # pandas grabs all data from masterlabresults
     df = pd.read_sql_query('select * from masterlabresults', con=conn)
 
+    # authorization into gsheets. Opens target sheet. Then sets data from query grabbed above into the sheet.
     gc = pygsheets.authorize(service_file=LOCAL_FILENAME)
     wks = gc.open("New 2019 Moto Lab Results").sheet1
     wks.set_dataframe(df, (1, 1))
